f49808cc23a0b0ec0859d0dbaee307094280cd49
sheetsnolonger <sam.personalt@gmail.com>
2026-06-20 13:38:21 -0400

    Update server.js

--- server.js
+++ server.js
@@ -79,7 +79,7 @@ app.use(express.static("public"));
 app.use(helmet({
   contentSecurityPolicy: {
     directives: {
-      defaultSrc: ["'self'"],
+      styleSrc: ["'self'", "'unsafe-inline'"],
       imgSrc: ["'self'", "https://files.catbox.moe", "data:"],
       mediaSrc: ["'self'"],
       styleSrc: ["'self'"],
